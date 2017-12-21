b5a3f9f5ad39d7a17af163b398b53dacf100f7b7
CODY <cody@github.com>
2017-12-21 12:02:26 -0800

    Second Upload

---
+++ djangoEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\cline\desktop\dojoassignments\python\django\beltreview\djangoenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
